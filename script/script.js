dfe80e4f49861c9c46b5097cdcf910732d5307ff
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2023-09-22 09:23:14 +0200

    bunch of code pls find my hidden const

---
+++ script/script.js
@@ -0,0 +1,66 @@
+const url = "http://127.0.0.1:3000/";
+
+const countryInput = document.querySelector("#country");
+const titleInput = document.querySelector("#title");
+const linkInput = document.querySelector("#link");
+const arrivalDateInput = document.querySelector("#arrival-date");
+const departureDateInput = document.querySelector("#departure-date");
+const imageInput = document.querySelector("#image-upload");
+const descriptionInput = document.querySelector("#description");
+const saveDestination = document.querySelector("#save-destination");
+
+async function addDestination(event) {
+  event.preventDefault();
+  const country = countryInput.value;
+  const title = titleInput.value;
+  const link = linkInput.value;
+  const arrivalDate = arrivalDateInput.value;
+  const departureDate = departureDateInput.value;
+  const image = imageInput.value;
+  const description = descriptionInput.value;
+
+  const obj = {
+    country,
+    title,
+    link,
+    arrivalDate,
+    departureDate,
+    image,
+    description,
+  };
+
+  console.log("obj", obj);
+  //   const dataArray=[]
+  //   dataArray.push(obj)
+
+  const response = await saveToDatabase(obj);
+  if (response.ok) {
+    const body = await response.json();
+    console.log("our body is:", body);
+    //   const newP = document.createElement("p");
+    //   newP.classList.add("user");
+    //   newP.title = newDateInRequestedFormat();
+    //   newP.id = body.name;
+    //   newP.innerText = message;
+    //   messages.appendChild(newP);
+    //   input.value = "";
+  }
+  if (response.status > 499) {
+    alert("Server not working");
+  } else if (response.status > 399) {
+    alert("User error");
+  } else {
+    console.log("all is good");
+  }
+}
+
+async function saveToDatabase(obj) {
+  const response = await fetch(url, {
+    method: "POST",
+    body: JSON.stringify(obj),
+  });
+  console.log("our response is:", response);
+  return response;
+}
+
+saveDestination.addEventListener("click", addDestination);
---
+++ server/server.js
@@ -0,0 +1,81 @@
+//server and database set up
+//add the server code
+const http = require("http");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { fileURLToPath } = require("url");
+const hostname = "127.0.0.1";
+const port = 3000;
+let connectionString = "mongodb://127.0.0.1:27017";
+const uri = connectionString;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function connectToDB(destinationData, res) {
+  try {
+    await client.connect();
+    const myDB = client.db("travelDestinations");
+    const myColl = myDB.collection("destinations");
+    const result = await addDataToDatabase(destinationData, res);
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+    res.writeHead(201, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ destination: result.insertedId }));
+  } catch (error) {
+    if (error.code === 11000) {
+      //handle duplicate key error
+      res.writeHead(400, { "Content-Type": "application/json" });
+      res.end(JSON.stringify({ error: "Data already exists." }));
+    } else {
+      console.error(error);
+      res.writeHead(500, { "Content-Type": "text-plain" });
+      res.end("Internal Server Error");
+    }
+  } finally {
+    await client.close();
+  }
+}
+
+//Inserts one doc in the database
+async function addDataToDatabase(destinationData, myColl) {
+  //once again the parsed body
+  const result = await myColl.insertOne(destinationData);
+  return result;
+}
+
+const findme = "hellooooo can you find me";
+
+//Create server
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  if (req.method === "POST") {
+    let body = "";
+    req.on("data", (chunk) => {
+      body += chunk.toString();
+    });
+
+    req.on("end", () => {
+      const parsedBody = JSON.parse(body);
+      connectToDB(parsedBody, res).catch((error) => {
+        //here we call the connectToDB function if we wanna post something & we set up an error catcher
+        console.error(error);
+        res.writeHead(500, { "Content-Type": "text-plain" });
+        res.end("Internal Server Error");
+      });
+    });
+  } else {
+    res.end("Hello");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
