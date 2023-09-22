e3d5d347bb8d8a1eb695d32994b49746a7f7f9eb
emkataumre <emo.vladinov@gmail.com>
2023-09-22 09:27:29 +0200

    hmm nothing will go wrong now im having such a good day

---
+++ script.js
@@ -0,0 +1,11 @@
+function factorial(n) {
+  if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const num = 5;
+const result = factorial(num);
+console.log(`The factorial of ${num} is ${result}`);
